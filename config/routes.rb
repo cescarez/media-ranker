defaccb571c46db23838544f8fc6524ae2958c8b
Christabel Sebastian <christabel.escarez@gmail.com>
2020-11-10 17:21:10 -0800

    added routes (though may want to rethink nested routes for Votes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "homepages#index"
+
+  resources :users do
+    resources :votes, only: [:create, :index, :show]
+  end
+  resources :works do
+    resources :votes, only: [:index, :show]
+  end
 end
