082be1a11ddfb6483f4f5432626f4a82e7b00d27
Christabel Sebastian <christabel.escarez@gmail.com>
2020-11-11 19:59:10 -0800

    Added custom methods for category and publication year validations

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,8 +1,27 @@
+VALID_CATEGORIES = ["album", "book", "movie"]
+
 class Work < ApplicationRecord
   has_many :votes
 
   validates :category, presence: true
   validates :title, presence: true
   validates :creator, presence: true
-  validates :publication_year, presence: true #TODO: if time, add date validation beyond presence
+  validates :publication_year, presence: true
+
+  def validate_category
+    unless VALID_CATEGORIES.include? self.category
+      raise ArgumentError, "Invalid category for work. Program exiting."
+    else
+      return self.category
+    end
+  end
+
+  def validate_publication_year
+    is_valid = self.publication_year.class == Date || self.publication_year.class == Time || self.publication_year.class == Datetime
+    unless is_valid
+      raise ArgumentError, "Invalid publication year for work. Program exiting"
+    else
+      return self.category
+    end
+  end
 end
