bf1cb4cc50d86664c864b6b40c72332a7f62d5a2
Christabel Sebastian <christabel.escarez@gmail.com>
2020-11-11 18:21:36 -0800

    user controller and tests written

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,81 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to user_path(@user.id)
+      return
+    else
+      flash.now[:error] =  "Error occurred. User did not save. Please try again."
+      render :new, status: :bad_request
+      return
+    end
+  end
+
+
+  def show
+    @user = User.find_by(id: params[:id])
+    if @user.nil?
+      render file: "#{Rails.root}/public/404.html", status: :not_found
+      return
+    end
+  end
+
+  def edit
+    @user = User.find_by(id: params[:id])
+
+    if @user.nil?
+      render file: "#{Rails.root}/public/404.html", status: :not_found
+      return
+    end
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+
+    if @user.nil?
+      render file: "#{Rails.root}/public/404.html", status: :not_found
+      return
+    elsif @user.update(user_params)
+      redirect_to user_path(@user.id)
+      return
+    else
+      flash.now[:error] =  "Error occurred. User did not update. Please try again."
+      render :edit, status: :bad_request
+      return
+    end
+  end
+
+  def destroy
+    @user = User.find_by(id: params[:id])
+
+    if @user.nil?
+      render file: "#{Rails.root}/public/404.html", status: :not_found
+      return
+    end
+    @user.votes.delete_all #is this necessary? TODO: investigate what happens when a user is deleted and view a work's list of votes
+
+    if @user.destroy
+      redirect_to users_path
+      return
+    else
+      flash[:error] =  "Error occurred. User did not delete. Please try again."
+      redirect_to user_path(@user.id), status: :bad_request
+      return
+    end
+  end
+
+  private
+
+  def user_params
+    return params.require(:user).permit(:name)
+  end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,128 @@
 require "test_helper"
 
 describe UsersController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  let (:user_hash) do
+    {user: {name: "Test book"}}
+  end
+
+  describe "index" do
+    it "responds with success when accessed" do
+      get users_path
+      must_respond_with :success
+    end
+  end
+
+  describe "new" do
+    it "responds with success when accessed" do
+      get new_user_path
+      must_respond_with :success
+    end
+  end
+
+  describe "create" do
+
+    it "posts a valid new user and redirects to associated show page" do
+      expect {
+        post users_path, params: user_hash
+      }.must_differ "User.count", 1
+
+      latest_user = User.last
+
+      must_redirect_to user_path(latest_user.id)
+
+      expect(latest_user[:name]).must_equal user_hash[:user][:name]
+    end
+
+    it "does not post an invalid new user and responds with bad_request" do
+      expect {
+        post users_path, params: {user: {name: nil}}
+      }.wont_change "User.count"
+
+      must_respond_with :bad_request
+    end
+  end
+
+  describe "show" do
+    it "responds with success when accessing a valid user" do
+      user = users(:user1)
+      get user_path(user.id)
+      must_respond_with :success
+    end
+
+    it "responds with not_found when accessing a non-existing user" do
+      get user_path(-1)
+      must_respond_with :not_found
+    end
+  end
+
+  describe "edit" do
+    it "responds with success for an existing, valid driver" do
+      user = users(:user1)
+      get edit_user_path(user.id)
+      must_respond_with :success
+    end
+
+    it "responds with redirect when getting the edit page for a non-existing user" do
+      get edit_user_path(-1)
+      must_respond_with :not_found
+    end
+  end
+
+  describe "update" do
+    it "successful save redirects to show page when updating an existing driver with valid params" do
+      user = users(:user1)
+      expect {
+        patch user_path(user.id), params: user_hash
+      }.wont_change "User.count"
+
+      must_redirect_to user_path(user.id)
+
+      user.reload
+
+      expect(user[:name]).must_equal user_hash[:user][:name]
+    end
+
+    it "responds with bad_request when attempting to update an existing driver with invalid params" do
+      user = users(:user1)
+
+      expect {
+        patch user_path(user.id), params: {user: {name: nil}}
+      }.wont_change "User.count"
+
+      must_respond_with :bad_request
+    end
+
+    it "responds with not_found when attempting to update an invalid driver with valid params" do
+      expect {
+        patch user_path(-1), params: user_hash
+      }.wont_change "User.count"
+
+      must_respond_with :not_found
+    end
+  end
+
+  describe "destroy" do
+    it "destroys an existing user then redirects" do
+      user = users(:user1)
+
+      expect {
+        delete user_path(user.id)
+      }.must_differ "User.count", -1
+
+      found_user = User.find_by(name: user.name)
+
+      expect(found_user).must_be_nil
+
+      must_redirect_to users_path
+
+    end
+
+    it "does not change the db when the driver does not exist, then responds with " do
+      expect{
+        delete user_path(-1)
+      }.wont_change "User.count"
+
+      must_respond_with :not_found
+    end
+  end
 end
