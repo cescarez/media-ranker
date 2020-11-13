d95bf1c618d409b5029a53dca5132f9975a830e2
Christabel Sebastian <christabel.escarez@gmail.com>
2020-11-12 19:11:37 -0800

    Added user login and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homepages#index"
 
-  resources :users do
+  resources :users do, except: [:new, :update, :edit]
     resources :votes, only: [:create, :index, :show]
   end
 
   get '/login', to: 'users#login', as: 'login_path'
-  # get '/logout', to: 'users#logout', as: 'logout_path'
+  post '/login', to: 'users#login'
+  post '/logout', to: 'users#logout', as: 'logout_path'
 
   resources :works do
     resources :votes, only: [:index, :show]
