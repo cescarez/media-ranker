51efe47fd68a80e95dee3e0518df24fe9a66cadb
Christabel Sebastian <christabel.escarez@gmail.com>
2020-11-13 10:24:37 -0800

    Removed nested routes and added a nickname to upvote route

--- config/routes.rb
+++ config/routes.rb
@@ -2,20 +2,18 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homepages#index"
 
-  resources :users, except: [:new, :update, :edit] do
-    resources :votes, only: [:create, :index, :show]
-  end
+
+  resources :users, except: [:new, :update, :edit]
 
   get '/users/current', to: 'users#current', as: 'current_user'
 
-  get '/login', to: 'users#login_form', as: 'login_path'
+  get '/login', to: 'users#login_form', as: 'login'
   post '/login', to: 'users#login'
-  post '/logout', to: 'users#logout', as: 'logout_path'
+  post '/logout', to: 'users#logout', as: 'logout'
 
-  resources :works do
-    resources :votes, only: [:index, :show]
-  end
+  resources :works
 
-  post '/works/:id/upvote', to: 'works#upvote'
+  post '/works/:id/upvote', to: 'works#upvote', as:
+    "upvote_work"
 
 end
