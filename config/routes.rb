00ec87b142d778d1234287201bfccf34f28209ba
Christabel Sebastian <christabel.escarez@gmail.com>
2020-11-12 19:27:26 -0800

    Changed syntax to fix user routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homepages#index"
 
-  resources :users do, except: [:new, :update, :edit]
+  resources :users, except: [:new, :update, :edit] do
     resources :votes, only: [:create, :index, :show]
   end
 
