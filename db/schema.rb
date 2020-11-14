558d7d34f52fe2b70c696e7ee32eec3dadae588f
Christabel Sebastian <christabel.escarez@gmail.com>
2020-11-13 18:53:31 -0800

    Removed user.join_date from schema with migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_13_060248) do
+ActiveRecord::Schema.define(version: 2020_11_14_025108) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.date "join_date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
