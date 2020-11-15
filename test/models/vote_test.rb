6f09bb00b65f0036eedd2a95bce7a6d817575bfc
Christabel Sebastian <christabel.escarez@gmail.com>
2020-11-14 19:44:17 -0800

    added vote fixtures (10 for each work in works.yml)

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -3,20 +3,16 @@ require "test_helper"
 #note: submit_date presence is not tested for as it gets assigned in Work#upvote
 #
 describe Vote do
-  let (:new_vote) do
-    work = works(:album1)
-    user = users(:user1)
-    Vote.new(user: user, work: work)
+  let (:vote) do
+    votes(:vote1)
   end
 
   describe "instantiation" do
     it "can be instantiated" do
-      expect(new_vote.valid?).must_equal true
+      expect(vote.valid?).must_equal true
     end
 
     it "will have the required fields" do
-      new_vote.save
-      vote = Vote.last
       [:work_id, :user_id].each do |field|
         expect(vote).must_respond_to field
       end
@@ -28,32 +24,27 @@ describe Vote do
     let (:other_user) { users(:user2) }
 
     it "has a work and user" do
-      new_vote.save
-      expect(new_vote.work).wont_be_nil
-      expect(new_vote.work_id).wont_be_nil
-      expect(new_vote.user).wont_be_nil
-      expect(new_vote.user_id).wont_be_nil
+      expect(vote.work).wont_be_nil
+      expect(vote.work_id).wont_be_nil
+      expect(vote.user).wont_be_nil
+      expect(vote.user_id).wont_be_nil
     end
 
-    it "changing the work and user  changes the associated foreign keys" do
-      new_vote.work = other_work
-      new_vote.user = other_user
+    it "changing the work and user changes the associated foreign keys" do
+      vote.work = other_work
+      vote.user = other_user
 
-      expect(new_vote.work_id).must_equal other_work.id
-      expect(new_vote.user_id).must_equal other_user.id
+      expect(vote.work_id).must_equal other_work.id
+      expect(vote.user_id).must_equal other_user.id
     end
 
     it "changing the work and user foreign keys changes the associated instances" do
-      new_vote.work_id = other_work.id
-      new_vote.user_id = other_user.id
+      vote.work_id = other_work.id
+      vote.user_id = other_user.id
 
-      expect(new_vote.work).must_equal other_work
-      expect(new_vote.user).must_equal other_user
+      expect(vote.work).must_equal other_work
+      expect(vote.user).must_equal other_user
     end
   end
-
-  # describe "custom methods" do
-  #
-  # end
 
 end
